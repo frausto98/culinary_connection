38fb1ce42a1d0e86e1eb25636710417c991cc350
frausto98 <frausto9298@yahoo.com>
2023-08-12 15:31:42 -0700

    added to the signup file. both login  and signups redirect  to welcome page due to authorization  not being valid

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -125,4 +125,13 @@ router.get('/login', (req, res) => {
     res.render('login');
 
 });
+
+router.get('/signup', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/home');
+        return;
+    }
+    res.render('signup');
+
+});
 module.exports = router;
--- utils/auth.js
+++ utils/auth.js
@@ -1,4 +1,6 @@
 const withAuth = (req, res, next) => {
+
+  //if you're not logged in, redirect to welcome page
     if (!req.session.loggedIn) {
       res.redirect('/')
     } else {
