4d69ea0694162edb2bfc125e90641c8223492315
unknown <samtisme@gmail.com>
2023-08-14 20:42:43 -0700

    changed seed javascript so that it properly builds the new users and recipies with the right model layout

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -12,8 +12,11 @@ const seedData = async () => {
         await sequelize.sync({ force: true });
         await sequelize.query('SET FOREIGN_KEY_CHECKS = 1')
        await user.create(data[0])
-       await recipe.create(data[1])
+       await user.create(data[1])
        await recipe.create(data[2])
+       await recipe.create(data[3])
+       await recipe.create(data[4])
+       await recipe.create(data[5])
        process.exit(0)
     } catch (error) {
         console.log(error)
