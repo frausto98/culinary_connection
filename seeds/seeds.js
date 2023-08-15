18547a367ff00b62e520521c3b4970ccb2b5d38e
BohoBabe <slenorovitz@gmail.com>
2023-08-15 14:23:13 -0700

    updating route names to match seeds route

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,9 +1,10 @@
 const data = require('./seeds.json');
-const recipe = require('../models/recipe');
-const user = require('../models/user');
-const ingredient = require('../models/ingredient');
-const steps = require('../models/step');
-const savedRecipe = require('../models/SavedRecipe');
+const { Recipe, User, Steps, Ingredient } = require('../models');
+// const recipe = require('../models/recipe');
+// const user = require('../models/user');
+// const ingredient = require('../models/ingredient');
+// const steps = require('../models/step');
+// const savedRecipe = require('../models/SavedRecipe');
 const sequelize = require('../config/connection');
 
 const seedData = async () => {
@@ -11,12 +12,12 @@ const seedData = async () => {
         await sequelize.query('SET FOREIGN_KEY_CHECKS = 0')
         await sequelize.sync({ force: true });
         await sequelize.query('SET FOREIGN_KEY_CHECKS = 1')
-       await user.create(data[0])
-       await user.create(data[1])
-       await recipe.create(data[2])
-       await recipe.create(data[3])
-       await recipe.create(data[4])
-       await recipe.create(data[5])
+       await User.create(data[0])
+       await User.create(data[1])
+       await Recipe.create(data[2])
+       await Recipe.create(data[3])
+       await Recipe.create(data[4])
+       await Recipe.create(data[5])
        process.exit(0)
     } catch (error) {
         console.log(error)
